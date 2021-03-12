2cfb12563f2fafcd97d33afdc0512bb0a9e57da4
tingfengsiyu <228613006@qq.com>
2021-03-12 18:04:19 +0800

    update k8s

--- api/v1/k8s/pod.go
+++ api/v1/k8s/pod.go
@@ -1,67 +1,181 @@
 package k8s
 
-//
-//import (
-//	"cmdb/utils"
-//	"flag"
-//	"fmt"
-//	"github.com/gin-gonic/gin"
-//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-//	"k8s.io/client-go/kubernetes"
-//	"k8s.io/client-go/tools/clientcmd"
-//	"os"
-//	"time"
-//)
-//
-//var ClientSet *kubernetes.Clientset
-//
-//func InitConfig(c *gin.Context) {
-//	var kubeconfig *string
-//	_, err := os.Lstat(utils.KubeFile)
-//	if err != nil {
-//		fmt.Sprintf("no such %s ", utils.KubeFile)
-//		panic("1")
-//	}
-//	kubeconfig = flag.String("kubeconfig", utils.KubeFile, "absolute path to the kubeconfig file")
-//	flag.Parse()
-//	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-//	if err != nil {
-//		panic(err.Error())
-//	}
-//	ClientSet, err = kubernetes.NewForConfig(config)
-//	if err != nil {
-//		panic(err.Error())
-//	}
-//}
-//
-//func ListPod(c *gin.Context) {
-//	for {
-//		// 通过实现 clientset 的 CoreV1Interface 接口列表中的 PodsGetter 接口方法 Pods(namespace string)返回 PodInterface
-//		// PodInterface 接口拥有操作 Pod 资源的方法，例如 Create、Update、Get、List 等方法
-//		// 注意：Pods() 方法中 namespace 不指定则获取 Cluster 所有 Pod 列表
-//		pods, err := ClientSet.CoreV1().Pods("").List(metav1.ListOptions{})
-//		if err != nil {
-//			panic(err.Error())
-//		}
-//		fmt.Printf("There are %d pods in the k8s cluster\n", len(pods.Items))
-//
-//		// 获取指定 namespace 中的 Pod 列表信息
-//		namespace := "default"
-//		pods, err = ClientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{})
-//		if err != nil {
-//			panic(err)
-//		}
-//		fmt.Printf("\nThere are %d pods in namespaces %s\n", len(pods.Items), namespace)
-//		for _, pod := range pods.Items {
-//			fmt.Printf("Name: %s, Status: %s, CreateTime: %s\n", pod.ObjectMeta.Name, pod.Status.Phase, pod.ObjectMeta.CreationTimestamp)
-//		}
-//		time.Sleep(10 * time.Second)
-//	}
-//}
-//
-//func ListDeployment(c *gin.Context) {
-//
-//}
+import (
+	"cmdb/utils"
+	"context"
+	"fmt"
+	"log"
+
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/retry"
+)
+
+type clientconfig struct {
+	clientset *kubernetes.Clientset
+}
+
+func int32Ptr(i int32) *int32 { return &i }
+
+func Initk8s() {
+	kubeconfig := utils.KubeFile
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	client := clientconfig{
+		clientset,
+	}
+	if err != nil {
+		panic(err.Error())
+	}
+	client.ListPod()
+}
+
+func (c *clientconfig) CreateDeployment(namespace, deploymentName, imageName string, replicas int32) {
+	deploymentsClient := c.clientset.AppsV1().Deployments(namespace)
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: deploymentName,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(replicas),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "demo",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "demo",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  "web",
+							Image: imageName,
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Create Deployment
+	fmt.Println("Creating deployment...")
+	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+
+}
+
+func (c *clientconfig) DeleteDeployment(namespace, deploymentName string) {
+	deploymentsClient := c.clientset.AppsV1().Deployments(namespace)
+	fmt.Println("Deleting deployment...")
+	deletePolicy := metav1.DeletePropagationForeground
+	if err := deploymentsClient.Delete(context.TODO(), deploymentName, metav1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	}); err != nil {
+		panic(err)
+	}
+	fmt.Println("Deleted deployment.")
+}
+
+func (c *clientconfig) UpdateDeployment(namespace, deploymentName, imageName string, replicas int32) {
+	deploymentsClient := c.clientset.AppsV1().Deployments(namespace)
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		// Retrieve the latest version of Deployment before attempting update
+		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
+		result, getErr := deploymentsClient.Get(context.TODO(), deploymentName, metav1.GetOptions{})
+		if getErr != nil {
+			panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
+		}
+
+		result.Spec.Replicas = int32Ptr(replicas)                 // reduce replica count
+		result.Spec.Template.Spec.Containers[0].Image = imageName // change nginx version
+		_, updateErr := deploymentsClient.Update(context.TODO(), result, metav1.UpdateOptions{})
+		return updateErr
+	})
+	if retryErr != nil {
+		panic(fmt.Errorf("Update failed: %v", retryErr))
+	}
+	fmt.Println("Updated deployment...")
+
+}
+
+func (c *clientconfig) ListDeployment(namespace string) {
+	deploymentList, _ := c.clientset.AppsV1().Deployments(namespace).List(context.TODO(), metav1.ListOptions{})
+	for _, deployment := range deploymentList.Items {
+		fmt.Println(deployment.Name, deployment.Namespace, deployment.Status.Replicas, deployment.Spec.Template.Spec.Containers[0].Image)
+	}
+}
+
+func (c *clientconfig) ListPod() {
+	namespace := "default"
+	pods, err := c.clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name, pod.Namespace, pod.Status.StartTime, pod.Status.Phase)
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+}
+
+func (c *clientconfig) ListNode() {
+
+	nodeList, err := c.clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, node := range nodeList.Items {
+		fmt.Printf("%s\t%s\t%s\t%s\n", node.Name, node.Status.Addresses, node.Status.NodeInfo.Architecture)
+	}
+}
+
+func (c *clientconfig) ListNamespace() {
+	namespaceList, _ := c.clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	for _, namespace := range namespaceList.Items {
+		fmt.Println(namespace.Name)
+	}
+
+}
+
+func (c *clientconfig) GetPod(namespace, pod string) {
+	_, err := c.clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+			pod, namespace, statusError.ErrStatus.Message)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+	}
+}
+
 //func ListStatefulSet(c *gin.Context) {
 //
 //}
