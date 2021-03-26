b2dc87966b13aa6d3f3352cea886087789394add
tingfengsiyu <228613006@qq.com>
2021-03-26 09:51:16 +0800

    update prometheus left join

--- model/Prometheus.go
+++ model/Prometheus.go
@@ -82,7 +82,7 @@ func HttpCheckExporter(client *http.Client, url string) int {
 func PrometheusServer() []ScanMonitorPrometheus {
 	var svc []ScanMonitorPrometheus
 	errs := db.Model(&Server{}).Select("monitor_prometheus.server_id,private_ip_address,node_export_port,process_export_port,script_export_port,node_export_status," +
-		"process_export_status,script_export_status,label,cluster,disable_node_export,disable_process_export,disable_script_export").Joins("left join monitor_prometheus on server.server_id=monitor_prometheus.server_id").Scan(&svc)
+		"process_export_status,script_export_status,label,cluster,disable_node_export,disable_process_export,disable_script_export").Joins("left join monitor_prometheus on server.server_id=monitor_prometheus.server_id").Scan(&svc).Error
 	if errs != nil {
 		middleware.SugarLogger.Errorf("", errs)
 		return svc
