76cf7d7bf8ddfb6cc1b038b2481e4626f3cf10c7
tingfengsiyu <228613006@qq.com>
2021-10-15 13:55:54 +0800

    update  readme and  init sql

--- README.md
+++ README.md
@@ -24,7 +24,7 @@
 
 ###  支持k8s集群管理
 
-
+###  支持webssh，堡垒机权限管理，日志审计
 
 ## 前端：
 
@@ -35,3 +35,66 @@
 ### 效果图如下
 
 ![效果](效果.png)
+
+### cmdb必须的目录结构
+```shell
+├── config
+│   ├── config.ini		#cmdb配置文件
+├── logs				#路径日志，可在cmdb中自定义
+│   ├── cmdb-err.log
+│   ├── cmdb.log
+├── web
+│   ├── dist			#前端存放位置
+├── main				# cmdb二进制程序
+```
+
+### cmdb配置文件
+
+``` shell
+# cat config/config.ini 
+[server]
+AppMode = release
+HttpPort = :5000		#运行端口
+JwtKey = 89js82js72
+LogFile = /data/ops/cmdb/logs/cmdb.log		#访问日志路径
+ErrorLogFile= logs/cmdb-err.log				#错误日志路径
+WorkerUser=ops								#批量操作的用户名
+WorkerPass=2IQ%N^GUJKWZ@09Y					#批量操作的用户密码
+WorkerSudoPass=2IQ%N^GUJKWZ@09Y				#批量操作的sudo用户密码
+SR_File_Max_Bytes=1048576
+ScriptDir=/data/ops/script/
+AnsibleHosts=/etc/ansible/hosts				#生成的机器列表
+AnsiblePlaybookDir=/mnt/roles				#页面上ansibleplaybook任务所在目录
+SshdPort=2222
+[database]									#数据库必须配置
+Db = mysql
+DbHost = 172.22.0.20
+DbPort = 3306
+DbUser = root
+DbPassWord = 123456 
+DbName = cmdb
+
+[ali]
+AccessKey = aaaaa
+SecretKey = 
+
+
+[aws]
+AccessKey =
+SecretKey =
+
+[k8s]
+KubeFile=config/testkube.txt
+
+[prometheus]
+prometheus_config_dir = /data/monitor/conf/prometheus/targets
+#被监控的机器ip所在目录，需要与prometheus配置文件相对应
+```
+
+### 数据库最好使用8.0
+
+使用database目录下的init.sql初始化数据库
+
+nohup ./main  &
+
+启动成功后，默认管理员账号密码为  luofeng 12345678910
--- datebase/init.sql
+++ datebase/init.sql
@@ -1,103 +1,234 @@
---
--- Host: 172.22.0.20    Database: cmdb
--- ------------------------------------------------------
--- Server version	8.0.23
+/*
+ Navicat Premium Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ Source Server         : 郫县
+ Source Server Type    : MySQL
+ Source Server Version : 50733
+ Source Host           : 30.10.0.18:3306
+ Source Schema         : cmdb
 
---
--- Table structure for table `user`
---
-create
-    database cmdb ;
-use
-    cmdb;
+ Target Server Type    : MySQL
+ Target Server Version : 50733
+ File Encoding         : 65001
+
+ Date: 15/10/2021 13:47:12
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for cabinet
+-- ----------------------------
+DROP TABLE IF EXISTS `cabinet`;
+CREATE TABLE `cabinet`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `idc_id` bigint(20) NOT NULL,
+  `cabinet_number_id` bigint(20) NOT NULL,
+  `cabinet_number` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_cabinet_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 85 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for cloud_instance
+-- ----------------------------
+DROP TABLE IF EXISTS `cloud_instance`;
+CREATE TABLE `cloud_instance`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `instance_id` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `host_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `status` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cpu` bigint(20) NOT NULL,
+  `memory` bigint(20) NOT NULL,
+  `os_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `region_id` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `instance_type` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `os_type` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `internet_max_bandwidth_in` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `start_time` datetime(3) DEFAULT NULL,
+  `expired_time` datetime(3) DEFAULT NULL,
+  `instance_creation_time` datetime(3) DEFAULT NULL,
+  `local_storage_capacity` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `private_ip_address` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `public_ip_address` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cloud` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_cloud_instance_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for idc
+-- ----------------------------
+DROP TABLE IF EXISTS `idc`;
+CREATE TABLE `idc`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `city` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `idc_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `idc_id` bigint(20) NOT NULL,
+  `cabinet_number_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_idc_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 79 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for monitor_prometheus
+-- ----------------------------
+DROP TABLE IF EXISTS `monitor_prometheus`;
+CREATE TABLE `monitor_prometheus`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `server_id` bigint(20) NOT NULL,
+  `node_export_port` bigint(20) DEFAULT 9100,
+  `process_export_port` bigint(20) DEFAULT 9256,
+  `script_export_port` bigint(20) DEFAULT 9172,
+  `node_export_status` bigint(20) DEFAULT 0,
+  `process_export_status` bigint(20) DEFAULT 0,
+  `script_export_status` bigint(20) DEFAULT 0,
+  `disable_node_export` bigint(20) DEFAULT 1,
+  `disable_process_export` bigint(20) DEFAULT 1,
+  `disable_script_export` bigint(20) DEFAULT 1,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `server_id`(`server_id`) USING BTREE,
+  INDEX `idx_monitor_prometheus_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 951 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for ops_records
+-- ----------------------------
+DROP TABLE IF EXISTS `ops_records`;
+CREATE TABLE `ops_records`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `user` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `object` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `action` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `state` bigint(20) NOT NULL DEFAULT 2,
+  `success` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `error` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_ops_records_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for server
+-- ----------------------------
+DROP TABLE IF EXISTS `server`;
+CREATE TABLE `server`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(42) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `models` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `location` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `private_ip_address` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `public_ip_address` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `label` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cluster` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `label_ip_address` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cpu` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `memory` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `disk` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `user` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `state` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `server_id` bigint(20) NOT NULL,
+  `idc_id` bigint(20) NOT NULL,
+  `cabinet_number_id` bigint(20) NOT NULL,
+  `gpu` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `mac` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `private_ip_address`(`private_ip_address`) USING BTREE,
+  UNIQUE INDEX `private_ip_address_2`(`private_ip_address`) USING BTREE,
+  UNIQUE INDEX `label_ip_address`(`label_ip_address`) USING BTREE,
+  INDEX `idx_server_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 496 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for term_log
+-- ----------------------------
+DROP TABLE IF EXISTS `term_log`;
+CREATE TABLE `term_log`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `protocol` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `term_user` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `client_ip` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `log` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `user` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `private_ip_address` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_term_log_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for term_user
+-- ----------------------------
+DROP TABLE IF EXISTS `term_user`;
+CREATE TABLE `term_user`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `port` bigint(20) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `identity_file` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `protocol` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `username`(`username`) USING BTREE,
+  UNIQUE INDEX `password`(`password`) USING BTREE,
+  INDEX `idx_term_user_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user`
-(
-    `id`         bigint unsigned NOT NULL AUTO_INCREMENT,
-    `created_at` datetime(3) DEFAULT NULL,
-    `updated_at` datetime(3) DEFAULT NULL,
-    `deleted_at` datetime(3) DEFAULT NULL,
-    `username`   varchar(20) NOT NULL,
-    `password`   varchar(20) NOT NULL,
-    `role`       bigint DEFAULT '2',
-    PRIMARY KEY (`id`),
-    KEY          `idx_user_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `user`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `role` bigint(20) DEFAULT 2,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_user_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `user`
---
+-- ----------------------------
+-- Table structure for user_permissions
+-- ----------------------------
+DROP TABLE IF EXISTS `user_permissions`;
+CREATE TABLE `user_permissions`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `server_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `term_user_id` bigint(20) NOT NULL,
+  `group` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_user_permissions_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1931 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-LOCK
-    TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+SET FOREIGN_KEY_CHECKS = 1;
 INSERT INTO `user`
 VALUES (4, '2021-01-26 15:22:12.588', '2021-01-26 15:22:12.588', NULL, 'luofeng', 'rytBNnf2J2vciy+8', 1);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK
-    TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-03-25 13:13:10
-
-CREATE TABLE `ops_records` (
-                               `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-                               `created_at` datetime(3) DEFAULT NULL,
-                               `updated_at` datetime(3) DEFAULT NULL,
-                               `deleted_at` datetime(3) DEFAULT NULL,
-                               `user` varchar(30) NOT NULL,
-                               `object` varchar(1000) DEFAULT NULL,
-                               `action` varchar(500) NOT NULL,
-                               `state` bigint(20) NOT NULL DEFAULT '2',
-                               `success` longtext NOT NULL DEFAULT 'success' ,
-                               `error` longtext NOT NULL DEFAULT 'error' ,
-                               PRIMARY KEY (`id`),
-                               KEY `idx_ops_records_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-CREATE TABLE `user_permissions` (   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,   `created_at` datetime(3) DEFAULT NULL,
-                                    `updated_at` datetime(3) DEFAULT NULL,   `deleted_at` datetime(3) DEFAULT NULL,
-                                    `server_id` bigint(20) NOT NULL,
-                                    `user_id` bigint(20) NOT NULL,
-                                    `term_user_id` bigint(20) NOT NULL,
-                                    `group` varchar(50) NOT NULL,
-                                    PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `term_user` (
-                             `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-                             `created_at` datetime(3) DEFAULT NULL,
-                             `updated_at` datetime(3) DEFAULT NULL,
-                             `deleted_at` datetime(3) DEFAULT NULL,
-                             `port` bigint(20) NOT NULL,
-                             `username` varchar(50) NOT NULL,
-                             `password` varchar(50) NOT NULL,
-                             `identity_file` varchar(50) NOT NULL,
-                             `protocol` varchar(50) NOT NULL,
-                             PRIMARY KEY (`id`),
-                             UNIQUE KEY `username` (`username`),
-                             UNIQUE KEY `password` (`password`),
-                             KEY `idx_term_user_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
-
